904e76b2cfd95430f514550e72b446e400890ccf
Ekta Kumari <ekta.a.kumari@capgemini.com>
2020-04-16 16:18:06 +0530

    data binding

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,21 @@
+
+export class Dish {
+
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean; 
+    label: string;
+    price: string;
+    description: string;
+    comments : Comment[] ;
+}
+
+export class Comment {
+
+    rating: number;
+    comment: string;
+    author: string;
+    date: string;
+}
